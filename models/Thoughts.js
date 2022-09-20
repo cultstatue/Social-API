1626fb329d369d2a28e3254b28170a276c6958eb
cultstatue <saltedchurros@gmail.com>
2022-09-20 15:26:17 -0400

    finished setting up models

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,17 +1,64 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
-const ThoughtsSchema = new Schema({
-
-    thoughtText: {
-        type: String,
-        required: true,
-        min: 1,
-        max: 280   
+const ReactionSchema = new Schema(
+    
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280
+        },
+        username: {
+            type: Schema.Types.ObjectId,
+            ref: User
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    {
+        toJSON: {
+          getters: true
+        }
     }
+);
+
+const ThoughtsSchema = new Schema(
+    
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280   
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: Schema.Types.ObjectId,
+            ref: User
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+          getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
 });
 
 const Thoughts = model('Thoughts', ThoughtsSchema);
